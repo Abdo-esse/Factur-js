712b3d24e5fed94d7e539f656a80addc7f63225c
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-01 16:05:15 +0100

    m

--- invoice.js
+++ invoice.js
@@ -7,13 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const invoiceTable = document.getElementById('invoiceTable').getElementsByTagName('tbody')[0];
     const invoiceTotal = document.getElementById('invoiceTotal');
     let quantite = document.querySelectorAll('.quantity')[0].value;
-    let quantite1 = document.querySelectorAll('.quantity')[1].value;
-    let quantite2 = document.querySelectorAll('.quantity')[2].value;
     let priec = document.querySelectorAll('.unitPrice')[0].value;
-    let priec1 = document.querySelectorAll('.unitPrice')[1].value;
-    let priec2 = document.querySelectorAll('.unitPrice')[2].value;
     
     
+    let X = document.getElementById("bnt");
+
+     
     
     // console.log(priec );
 
@@ -22,14 +21,21 @@ document.addEventListener('DOMContentLoaded', function() {
     
     
     // This function updates the total amount in each row ( item price * Qte )
-    function updateItemTotal(row) {
+    // quantite.onchange = function (ev) {
+    
+        
+        
        
-    }
+    // }
 
 
     // This function updates the total amount of the invoice (sum all Item totals)
     function updateInvoiceTotal() {
-       
+        let Y;
+        Y = priec * quantite;
+        console.log(Y);
+        
+        X.textContent = Y;
     }
 
 
